78c1ba6005b5615d2a2a4638766e3020e576a0bc
Gabriel Ribeiro Rossi <gabriel.rossi@sebsa.com.br>
2025-04-23 14:25:36 -0300

    feat initial project

---
+++ Api.GRRInnovations.SmartInventory/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.GRRInnovations.SmartInventory.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            // Simulate fetching products from a database or service
+            var products = new List<string> { "Product1", "Product2", "Product3" };
+            return Ok(products);
+        }
+    }
+}
---
+++ Api.GRRInnovations.SmartInventory/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace Api.GRRInnovations.SmartInventory
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
